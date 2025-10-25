e4b66911216b0a3da3a434c741d9542799a83a7c
RenggoPandora <renggopandora07@gmail.com>
2025-10-25 16:25:45 +0700

    feat: CCTV availability handling

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
             RadioSeeder::class,
             HpbocSeeder::class,
             PcDeviceSeeder::class,
+            CctvSeeder::class,
             TicketSeeder::class,
         ]);
     }
